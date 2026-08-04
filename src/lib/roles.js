91c79e49c7e09a26092d0e539aee46410f77352e
Ahyan <ahyannubaid1003@gmail.com>
2026-08-04 23:22:44 +0700

    Update UID akun Ojek setelah reset akun

--- src/lib/roles.js
+++ src/lib/roles.js
@@ -1,6 +1,6 @@
 import { ROLE } from './constants'
 
 export const ROLE_BY_UID = {
-  CWkRb5X55HT3PmoO4kkcPjVGXUf1: ROLE.OJEK, // ahyannubaid1003@gmail.com
+  eHCzWlBeNtf97Ht4pcSEH72qHdA3: ROLE.OJEK, // ahyan@gmail.com (akun baru)
   EsGIzSV9HLXx1rd0Bh6H3J7C3Hr2: ROLE.PENUMPANG, // fajri@gmail.com (tester)
 }
