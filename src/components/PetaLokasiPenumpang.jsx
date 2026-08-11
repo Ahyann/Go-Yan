d6c20d7ca32302e186734ad66498f16cbc8501a2
Ahyan <ahyannubaid1003@gmail.com>
2026-08-11 21:54:49 +0700

    Kecilkan ukuran icon Fajri di peta Ahyan

--- src/components/PetaLokasiPenumpang.jsx
+++ src/components/PetaLokasiPenumpang.jsx
@@ -14,14 +14,14 @@ const ikonPenumpang = L.divIcon({
   className: '',
   html: `<div style="isolation: isolate;">
     <img src="/icons/fajri.png" style="
-      width:21px;height:32px;
+      width:16px;height:24px;
       image-rendering: pixelated;
       filter: drop-shadow(0 0 6px #E8EDF9) drop-shadow(0 0 10px #8B96B4);
     " />
   </div>`,
-  iconSize: [21, 32],
-  iconAnchor: [11, 32],
-  popupAnchor: [49, -14],
+  iconSize: [16, 24],
+  iconAnchor: [8, 24],
+  popupAnchor: [37, -10],
 })
 
 const ikonSaya = L.divIcon({
